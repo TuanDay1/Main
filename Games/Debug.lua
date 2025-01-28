d194713833f0baa3d6fbed1e68e85c7dd31ab2bb
phanphugame <gaming190807@gmail.com>
2025-01-28 18:39:00 +0700

    Update

--- Games/Debug.lua
+++ Games/Debug.lua
@@ -42,6 +42,12 @@ local debugScripts = {
 local ScriptNames = {}
 for index, _ in pairs(debugScripts) do table.insert(ScriptNames, index) end
 
+local antiScripts = {
+    ["Anti Adonis"] = "https://raw.githubusercontent.com/C0RP0RATE/Oblivious/refs/heads/main/Adonis_AntiCheat_Bypass_V2.lua",
+}
+local AntiNames = {}
+for index, _ in pairs(antiScripts) do table.insert(AntiNames, index) end
+
 local main_Tab = window:CreateTab("Chính", "rbxassetid://113518381337162")
 UILib:SelectTab("Chính")
 main_Tab:Dropdown(
@@ -55,13 +61,24 @@ main_Tab:Dropdown(
         UILib:Notify("Script", "Đã khởi chạy đoạn mã thành công!", nil, 5)
     end
 )
+main_Tab:Dropdown(
+    "Chạy anti:",
+    AntiNames,
+    function(value: string)
+        if value == "" then return end
+        if antiScripts[value] == nil then return end
+
+        loadstring(_httpget(game, antiScripts[value]))()
+        UILib:Notify("Script", "Đã khởi chạy đoạn mã thành công!", nil, 5)
+    end
+)
 main_Tab:Button(
     "Sao chép vị trí nhân vật",
     function()
         local playerCharacter = player.Character or player.CharacterAdded:Wait()
         local playerHumanoidRootPart = playerCharacter:WaitForChild("HumanoidRootPart")
 
-        _setclipboard(playerHumanoidRootPart.CFrame)
+        _setclipboard(string.format("CFrame.new(%s, %s, %s)", playerHumanoidRootPart.CFrame.X, playerHumanoidRootPart.CFrame.Y, playerHumanoidRootPart.CFrame.Z))
         UILib:Notify("Character", "Đã sao chép vị trí nhân vật vào bộ nhớ đệm của bạn", nil, 5)
     end
 )
