c12beb1e798ed1cba625b01bbf1b330f06cf7716
phanphugame <gaming190807@gmail.com>
2025-01-28 08:42:33 +0700

    Update Fisch

--- Games/Fisch.lua
+++ Games/Fisch.lua
@@ -13,6 +13,8 @@ local FileManager = loadstring(_httpget(game, "https://raw.githubusercontent.com
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TeleportService = game:GetService("TeleportService")
+local HttpService = game:GetService("HttpService")
+local GuiService = game:GetService("GuiService")
 
 ----------Variables----------
 local player = Players.LocalPlayer
@@ -24,16 +26,15 @@ local window, fileName = UILib:CreateWindow("Fisch", "NTCHub")
 local function updateSettting(index, value)
     local keys = string.split(index, "/")
     local current = _env.Config
-    
+
     if #keys == 1 then
         current[index] = value
     else
         for i = 1, #keys - 1 do
-            if current[keys[i]] then
-                current = current[keys[i]]
-            else
-                return
+            if current[keys[i]] == nil then
+                current[keys[i]] = {}
             end
+            current = current[keys[i]]
         end
         local finalKey = keys[#keys]
         current[finalKey] = value
@@ -54,12 +55,21 @@ end
 
 ]]--
 
+GuiService.ErrorMessageChanged:Connect(function()
+    if GuiService:GetErrorCode() == Enum.ConnectionError.DisconnectLuaKick then
+        if _env.Config["Auto Rejoin"] == true then
+            TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId)
+            return
+        end
+    end
+end)
+
 playerGui.ChildAdded:Connect(function(child: Instance)
     if child.Name == "reel" then
         if _env.Config["Auto Reel"] == true then
-            task.wait(math.random(1,3))
-            local reelfinished = ReplicatedStorage:WaitForChild("events"):FindFirstChild("reelfinished")
-            reelfinished:FireServer(100, true)
+            task.wait(math.random(2,4))
+            local reelfinished = ReplicatedStorage:WaitForChild("events"):FindFirstChild("reelfinished ")
+            reelfinished:FireServer(math.random(91,100), true)
         end
     end
 end)
@@ -77,15 +87,48 @@ main_Tab:Toggle(
 )
 
 ---------------------------------------------------------- MORE
--- local more_Tab = window:CreateTab("Thêm", "rbxassetid://113518381337162")
--- more_Tab:Toggle(
---     "Tham gia lại máy chủ",
---     "Tham gia lại máy chủ này",
---     false,
---     function(value: boolean)
---         TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId)
---     end
--- )
+local more_Tab = window:CreateTab("Thêm", "rbxassetid://113518381337162")
+more_Tab:Toggle(
+    "Tự động tham gia lại (Bị đuổi)",
+    "Tự động tham gia lại nếu như bị trò chơi đuổi",
+    _env.Config["Auto Rejoin"] or false,
+    function(value)
+        updateSettting("Auto Rejoin", value)
+    end
+)
+local function rejoinTimer()
+    while _env.Config["Rejoin Timer"].Enable do
+        task.wait(_env.Config["Rejoin Timer"].Minute * 60)
+        if _env.Config["Rejoin Timer"].Enable == false then break end
+        TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId)
+    end
+end
+more_Tab:Toggle(
+    "Tự động tham gia lại (Thời gian)",
+    "Tự động tham gia lại sau khi đã hết thời gian đã đặt trước đó",
+    (_env.Config["Rejoin Timer"] and _env.Config["Rejoin Timer"]["Enable"]) or false,
+    function(value)
+        updateSettting("Rejoin Timer/Enable", value)
+        if value == true then
+            rejoinTimer()
+        end
+    end
+)
+more_Tab:Slider(
+    "Đặt thời gian tự động tham gia lại",
+    (_env.Config["Rejoin Timer"] and _env.Config["Rejoin Timer"]["Minute"]) or 60,
+    5,
+    300,
+    function(value)
+        updateSettting("Rejoin Timer/Minute", value)
+    end
+)
+more_Tab:Button(
+    "Tham gia lại máy chủ",
+    function()
+        TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId)
+    end
+)
 
 ---------------------------------------------------------- SETTINGS
 local setting_Tab = window:CreateTab("Cài đặt", "rbxassetid://113518381337162")
@@ -115,58 +158,61 @@ setting_Tab:Toggle(
         minifyCopy = value
     end
 )
-local function filterConfig(config)
-    local filtered = {}
-    for key, value in pairs(config) do
-        if type(value) == "table" then
-            filtered[key] = filterConfig(value)
-        elseif value ~= false then
-            filtered[key] = value
-        end
-    end
-    return filtered
-end
-local function serializeTable(tbl, indent)
-    indent = indent or 2
-    local result = {}
-    local spacing = string.rep("  ", indent)
-    
-    for key, value in pairs(tbl) do
-        if type(key) == "string" then
-            key = '"' .. key .. '"'
-        end
-        
-        if type(value) == "table" then
-            table.insert(result, spacing .. "[" .. key .. "] = {")
-            table.insert(result, serializeTable(value, indent + 1))
-            table.insert(result, spacing .. "},")
-        else
-            if value == "" or value == nil then
-                table.insert(result, spacing .. "[" .. key .. "] = '',")
-            else
-                table.insert(result, spacing .. "[" .. key .. "] = " .. tostring(value) .. ",")
+setting_Tab:Button(
+    "Sao chép tùy chỉnh vào bộ nhớ đệm",
+    function()
+        local function filterConfig(config)
+            local filtered = {}
+            for key, value in pairs(config) do
+                if type(value) == "table" then
+                    filtered[key] = filterConfig(value)
+                elseif value ~= false then
+                    filtered[key] = value
+                end
             end
+            return filtered
         end
+        local function serializeTable(tbl, indent)
+            indent = indent or 2
+            local result = {}
+            local spacing = string.rep("  ", indent)
+            
+            for key, value in pairs(tbl) do
+                if type(key) == "string" then
+                    key = '"' .. key .. '"'
+                end
+                
+                if type(value) == "table" then
+                    table.insert(result, spacing .. "[" .. key .. "] = {")
+                    table.insert(result, serializeTable(value, indent + 1))
+                    table.insert(result, spacing .. "},")
+                else
+                    if value == "" or value == nil then
+                        table.insert(result, spacing .. "[" .. key .. "] = '',")
+                    else
+                        table.insert(result, spacing .. "[" .. key .. "] = " .. tostring(value) .. ",")
+                    end
+                end
+            end
+        
+            return table.concat(result, "\n")
+        end
+
+        local result = ""
+        if filterCopy then
+            local filtered = filterConfig(_env.Config)
+            result = filtered
+        else
+            result = _env.Config
+        end
+
+        if minifyCopy then
+            _setclipboard("getgenv().Config = "..HttpService:JSONEncode(_env.Config))
+        else
+            local serializedString = "{" .. "\n" .. serializeTable(result) .. "\n" .. "}"
+            _setclipboard("getgenv().Config = "..serializedString)
+        end
+
+        UILib:Notify("Config", "Đã sao chép cài đặt vào bộ nhớ đệm của bạn", nil, 5)
     end
-
-    return table.concat(result, "\n")
-end
--- setting_Tab:Button("Sao chép tùy chỉnh vào bộ nhớ đệm", function()
---     local result = ""
-
---     if filterCopy then
---         local filtered = filterConfig(hubConfig)
---         result = filtered
---     else
---         result = hubConfig
---     end
-
---     if minifyCopy then
---         _setclipboard("getgenv().Config = "..HttpService:JSONEncode(hubConfig))
---     else
---         local serializedString = "{" .. "\n" .. serializeTable(result) .. "\n" .. "}"
---         _setclipboard("getgenv().Config = "..serializedString)
---     end
-
---     window:SendNotification("Đã sao chép cài đặt vào bộ nhớ đệm của bạn", nil, 5)
--- end)
+)
